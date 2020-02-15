3da6800e24d088a1b55b409d9c5b9d4d0df2f1d8
Waruna-Kaushalya <waruna95610@gmail.com>
2020-02-15 18:37:34 +0530

    Create add event view controller

--- Controllers/Home/AddEventViewController.swift
+++ Controllers/Home/AddEventViewController.swift
@@ -7,23 +7,113 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
+import FirebaseStorage
 
 class AddEventViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventImagePicker: UIImageView!
     @IBOutlet weak var eventImage: UIStackView!
     @IBOutlet weak var eventTitle: UITextField!
     @IBOutlet weak var eventDescription: UITextField!
     @IBOutlet weak var publishButton: UIButton!
+    
+    let alertMSG = AlertMessages()
+    let trancVc = TransitionController()
+    
+    var image: UIImage? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setElement()
-
-    
+        setupProfileImage()
+        
+        
     }
     func setElement() {
         eventTitle.styleTextField()
         eventDescription.styleTextField()
         publishButton.styleButton()
     }
-
+    func setupProfileImage(){
+        
+        eventImage.layer.cornerRadius = 20
+        eventImage.clipsToBounds = true
+        //        testImagePicker.isUserInteractionEnabled = true
+        eventImage.isUserInteractionEnabled = true
+        let tapGester = UITapGestureRecognizer(target: self, action: #selector(presentPicker))
+        eventImage.addGestureRecognizer(tapGester)
+        
+    }
+    @objc func presentPicker(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    func validateFields() -> String? {
+        
+        //check that all fields are field in
+        
+        if eventTitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || eventDescription.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        {
+            
+            alertMSG.warningAlertMessage(_AlertMessage: EMPTY_FIELDS, _viewCFrom: self)
+            return EMPTY_FIELDS
+            
+        }
+        
+        return nil
+    }
+    
+    
+    
+    @IBAction func publishedButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
+
+
+extension AddEventViewController:UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as?
+            UIImage{
+            image = imageSelected
+            eventImagePicker.image = imageSelected
+            eventImagePicker.image = imageSelected
+            
+        }
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as?
+            UIImage{
+            image = imageOriginal
+            eventImagePicker.image = imageOriginal
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+    //        self.navigationController?.navigationBar.shadowImage = UIImage()
+    //
+    //
+    //        super.viewDidAppear(animated)
+    //        let height: CGFloat = 30 //whatever height you want to add to the existing height
+    //        let bounds = self.navigationController!.navigationBar.bounds
+    //        self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height + height)
+    //
+    //
+    //    }
+    
+    
+}
+
+
+
+
+
